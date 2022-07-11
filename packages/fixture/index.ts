4a549aeede945f22826d97d453c8b27e05e28877
masterX89 <xhk1995@gmail.com>
2022-07-11 14:51:20 +0800

    feat: add eslint-config-ts

--- packages/fixture/index.ts
+++ packages/fixture/index.ts
@@ -1,8 +1,16 @@
 // eslint-disable-next-line no-console
 console.log('aaa')
 
-let a = 1
+// eslint-disable-next-line @typescript-eslint/no-inferrable-types
+let a: number = 1
 if (a === 1) a = 2
 if (a === 2) {
   a = 3
+}
+
+// @ts-expect-error requires a comment
+export const b: string = 1
+
+export interface Foo {
+  a: string
 }
