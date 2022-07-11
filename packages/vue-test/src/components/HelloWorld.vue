e1a4d7b5ab8dcb044d9398464661b398e75cd051
masterX89 <xhk1995@gmail.com>
2022-07-11 17:28:22 +0800

    feat: add eslint-config-vue

--- packages/vue-test/src/components/HelloWorld.vue
+++ packages/vue-test/src/components/HelloWorld.vue
@@ -24,7 +24,9 @@ const count = ref(0)
     <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Documentation</a>
   </p>
 
-  <button type="button" @click="count++">count is: {{ count }}</button>
+  <button type="button" @click="count++">
+    count is: {{ count }}
+  </button>
   <p>
     Edit
     <code>components/HelloWorld.vue</code> to test hot module replacement.
